31c5291eac52b1726f3898bf32cb0e982a11a1f1
Anthon002 <chineduanulugwo@gmail.com>
2023-05-16 01:37:27 -0700

    task 0

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+extern char **environ;
+
+#define MAX_COMMAND_LENGTH 2048
+
+#endif
